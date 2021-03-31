93b9e22adb7112d62fd2834aa793a9230777de94
OdeliaFink <odelia.finkel@gmail.com>
2021-03-31 19:20:40 +0000

    Added Display Showing Total Inventory and All Categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def show
+    @product_num = Product.sum(:quantity)
+    @category_num = Category.count
   end
 end
